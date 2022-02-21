5c2b9c7b88fd39d96735c332a606be41099a9c91
ionutpotolea <ionut.potolea@gmail.com>
2022-02-21 19:46:40 +0200

    hooked API to the form

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const noSearch = document.querySelector('.no-search')
 const searchField = document.getElementById('search-field')
-
+const searchForm = document.getElementById('search-form')
 
 noSearch.addEventListener("click", () => searchField.focus())
+searchForm.addEventListener("submit", searchFilm)
+
+let omdbApiParams = {
+    plot: "full",
+    apikey: "aad72dfe",
+    r: "JSON",
+    type: "movie",
+    s: ""
+}
+
+function convertToQueryString(params){
+    const qs = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+    .join('&');
+    return qs
+}
+
+function searchFilm(e){
+    e.preventDefault()
+    omdbApiParams = {
+        ...omdbApiParams,
+        s: searchField.value
+    }
+    let qs = convertToQueryString(omdbApiParams)
+    fetch(`http://www.omdbapi.com/?${qs}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    searchField.value = ""
+}
